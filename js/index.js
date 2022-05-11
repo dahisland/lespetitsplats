0403ccc7c463c4ed55dbd22412688692c23493fa
Myriam Mornet <dahisland@gmail.com>
2022-05-12 01:29:18 +0200

    First functional version of search by tag (working with search by search bar)

--- js/index.js
+++ js/index.js
@@ -118,6 +118,19 @@ const tagsSelectedContainer = document.querySelector(".nav_selectedTags");
 let allTags = document.querySelectorAll(".selectedTags_item--unchecked");
 let arrayTagsSelected = [];
 
+// Update tags list displays
+function updateTagsLists() {
+  containerTagsIngredient.innerHTML = "";
+  containerTagsAppliance.innerHTML = "";
+  containerTagsUstensil.innerHTML = "";
+
+  arrayRecipesFiltered.forEach((element) => {
+    tags.displayTagsLists(element, containerTagsIngredient, objTagsIngredient);
+    tags.displayTagsLists(element, containerTagsAppliance, objTagsAppliance);
+    tags.displayTagsLists(element, containerTagsUstensil, objTagsUstensil);
+  });
+}
+
 function filterRecipeByTag() {
   const filterRecipes = (obj) => {
     const testEachSearchWord = (item) => {
@@ -142,114 +155,125 @@ function filterRecipeByTag() {
   return arrayRecipesFiltered;
 }
 
-allTags.forEach((tag) => {
-  tag.addEventListener("click", () => {
-    let containerTagSelected = document.createElement("li");
-    if (tag.getAttribute("data-status") == "unchecked") {
-      arrayTagsSelected.push(norm.getNormalizeText(tag.textContent));
-      tags.selectTag(containerTagSelected, tag);
-      tagsSelectedContainer.appendChild(containerTagSelected);
-    }
+function eventBtnClose(tag) {
+  let allIconsClose = document.querySelectorAll(
+    ".nav_selectedTags > li > span"
+  );
 
-    // filtre des tags
-    filterRecipeByTag();
+  allIconsClose.forEach((icon) => {
+    icon.addEventListener("click", () => {
+      if (icon.parentNode.getAttribute("data-value") == tag.textContent) {
+        tag.setAttribute("data-status", "unchecked");
+        tag.classList.remove("selectedTags_item--checked");
+        tag.classList.add("selectedTags_item--unchecked");
+      }
+      icon.parentNode.remove();
 
-    // Update tags list displays
-    containerTagsIngredient.innerHTML = "";
-    containerTagsAppliance.innerHTML = "";
-    containerTagsUstensil.innerHTML = "";
+      delete arrayTagsSelected[
+        arrayTagsSelected.indexOf(
+          norm.getNormalizeText(icon.parentNode.textContent)
+        )
+      ];
 
-    arrayRecipesFiltered.forEach((element) => {
-      tags.displayTagsLists(
-        element,
-        containerTagsIngredient,
-        objTagsIngredient
-      );
-      tags.displayTagsLists(element, containerTagsAppliance, objTagsAppliance);
-      tags.displayTagsLists(element, containerTagsUstensil, objTagsUstensil);
-    });
-
-    //
-    let allIconsClose = document.querySelectorAll(
-      ".nav_selectedTags > li > span"
-    );
-
-    allIconsClose.forEach((icon) => {
-      icon.addEventListener("click", () => {
-        if (icon.parentNode.getAttribute("data-value") == tag.textContent) {
-          tag.setAttribute("data-status", "unchecked");
-          tag.classList.remove("selectedTags_item--checked");
-          tag.classList.add("selectedTags_item--unchecked");
-        }
-        icon.parentNode.remove();
-
-        delete arrayTagsSelected[
-          arrayTagsSelected.indexOf(norm.getNormalizeText(tag.textContent))
-        ];
-
-        const filterRecipes = (obj) => {
-          const testEachSearchWord = (item) => {
-            return obj.tags.includes(item) == true;
-          };
-          return arrayTagsSelected.every(testEachSearchWord);
+      const filterRecipes = (obj) => {
+        const testEachSearchWord = (item) => {
+          return obj.tags.includes(item) == true;
         };
-        containerRecipes.innerHTML = "";
-        arrayRecipesFiltered = arrayRecipes.filter(filterRecipes);
+        return arrayTagsSelected.every(testEachSearchWord);
+      };
+      containerRecipes.innerHTML = "";
+      arrayRecipesFiltered = arrayRecipes.filter(filterRecipes);
+
+      if (searchInput.value.length > 2) {
+        let normalizeSearchUser = norm
+          .getNormalizeText(searchInput.value)
+          .trim();
+        let regexWord = /([0-9a-z]{0,}\ ?)/g;
+        let wordsSearchedArray = normalizeSearchUser.match(regexWord);
+
+        algo.searchInRecipes(
+          containerRecipes,
+          wordsSearchedArray,
+          arrayRecipesFiltered
+        );
+        arrayRecipesFiltered = algo.searchInRecipes(
+          containerRecipes,
+          wordsSearchedArray,
+          arrayRecipesFiltered
+        );
+      } else {
         arrayRecipesFiltered.forEach((arr) => {
           containerRecipes.appendChild(arr.li);
         });
-      });
+        arrayRecipesFiltered = arrayRecipes.filter(filterRecipes);
+      }
+      updateTagsLists();
     });
-
-    // Update tags list displays
-    //   containerTagsIngredient.innerHTML = "";
-    //   containerTagsAppliance.innerHTML = "";
-    //   containerTagsUstensil.innerHTML = "";
-
-    //   arrayRecipesFiltered.forEach((element) => {
-    //     tags.displayTagsLists(
-    //       element,
-    //       containerTagsIngredient,
-    //       objTagsIngredient
-    //     );
-    //     tags.displayTagsLists(element, containerTagsAppliance, objTagsAppliance);
-    //     tags.displayTagsLists(element, containerTagsUstensil, objTagsUstensil);
-    //   });
   });
-});
+  return arrayRecipesFiltered;
+}
+
+function searchByTag() {
+  allTags.forEach((tag) => {
+    tag.addEventListener("click", () => {
+      let containerTagSelected = document.createElement("li");
+
+      // Style for tags selected
+      if (tag.getAttribute("data-status") == "unchecked") {
+        arrayTagsSelected.push(norm.getNormalizeText(tag.textContent));
+        tags.selectTag(containerTagSelected, tag);
+        tagsSelectedContainer.appendChild(containerTagSelected);
+      }
+
+      // Recipes filtered by tags and updating tags lists
+      filterRecipeByTag();
+      updateTagsLists();
+
+      arrayRecipesFiltered = filterRecipeByTag();
+
+      // Filtering recipes on click button close tag selected
+      eventBtnClose(tag);
+      arrayRecipesFiltered = eventBtnClose(tag);
+    });
+  });
+  return arrayRecipesFiltered;
+}
+
+searchByTag();
 
 // ------------------------------------------------------------------------------------------- //
 // ---------------------------------------------------- CALL NATIVE SEARCH ALGO ON EVENT INPUT //
 // ------------------------------------------------------------------------------------------- //
+function searchBySearchBar() {
+  searchInput.addEventListener("input", (e) => {
+    let searchUser = e.target.value;
+    let normalizeSearchUser = norm.getNormalizeText(searchUser).trim();
+    // Create array containing each word searched by user
+    let regexWord = /([0-9a-z]{0,}\ ?)/g;
+    let wordsSearchedArray = normalizeSearchUser.match(regexWord);
 
-searchInput.addEventListener("input", (e) => {
-  let searchUser = e.target.value;
-  let normalizeSearchUser = norm.getNormalizeText(searchUser).trim();
-  // Create array containing each word searched by user (test)
-  let regexWord = /([0-9a-z]{0,}\ ?)/g;
-  let wordsSearchedArray = normalizeSearchUser.match(regexWord);
-  // end test
-
-  // Update cards recipes displays
-
-  algo.searchInRecipes(containerRecipes, wordsSearchedArray, arrayRecipes);
-
-  arrayRecipesFiltered = algo.searchInRecipes(
-    containerRecipes,
-    wordsSearchedArray,
-    arrayRecipes
-  );
-  // Update tags list displays
-  containerTagsIngredient.innerHTML = "";
-  containerTagsAppliance.innerHTML = "";
-  containerTagsUstensil.innerHTML = "";
-
-  arrayRecipesFiltered.forEach((element) => {
-    tags.displayTagsLists(element, containerTagsIngredient, objTagsIngredient);
-    tags.displayTagsLists(element, containerTagsAppliance, objTagsAppliance);
-    tags.displayTagsLists(element, containerTagsUstensil, objTagsUstensil);
+    // Update cards recipes displays
+    if (arrayTagsSelected[0] == undefined) {
+      algo.searchInRecipes(containerRecipes, wordsSearchedArray, arrayRecipes);
+      arrayRecipesFiltered = algo.searchInRecipes(
+        containerRecipes,
+        wordsSearchedArray,
+        arrayRecipes
+      );
+      updateTagsLists();
+    } else {
+      arrayRecipesFiltered = searchByTag();
+      algo.searchInRecipes(
+        containerRecipes,
+        wordsSearchedArray,
+        arrayRecipesFiltered
+      );
+      updateTagsLists();
+    }
   });
-});
+  return arrayRecipesFiltered;
+}
+searchBySearchBar();
 
 // ------------------------------------------------------------------------------------------- //
 // ------------------------------------------------------------------------------- SEARCH TAGS //
